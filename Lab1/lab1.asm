ecf0f50c7d0b268a8c38bd930d9c05821fa81bd1
Raoul Nicolaescu <raoulg13@yahoo.com>
2024-02-12 12:21:19 +0200

    actually added lab1

--- Lab1/lab1.asm
+++ Lab1/lab1.asm
@@ -1,21 +1,47 @@
-ASSUME cs: code, ds:data
-; data - segmentul de date in care vom defini variabilele
+;z=1/(a*a+b*b-5)+2/(a*a-b*b+4)
+assume cs:text,ds:data
 data SEGMENT
-a db 7 ; a este un octet(byte)
-b dw 10 ; b este un cuvant (word -2 bytes)
-c db 3,5,6,1 ; c e un sir de octeti
+
+a db 2
+b db 3
+
+z dw ?
+
+
 data ENDS
-;code - numele segmentului de cod
-code SEGMENT
-start:
-mov ax,data ; pentru a sti care e segmentul de date, mutam in registrul ds
-mov ds,ax ; adresa segmentului de date, si pentru ca nu putem face asta direct folosim intermediar registrul ax
-; .......
-;aici avem instructiunile programului nostru
-;........
-mov ax,4C00h
-int 21h
-; e apelata intreruperea 21, cu ah incarcat cu 4C,adica sfarsitul executiei, si se returneaza codul de eroare al=00, adica
-; executie cu succes
-code ENDS
+text SEGMENT
+
+	start:
+
+	MOV AX, data
+	MOV DS, AX
+
+	MOV AL, a
+	MUL a
+	MOV BX, AX
+	MOV AL, b
+	MUL b
+	ADD AX, BX
+	SUB AX, 5
+	MOV BX, AX
+	MOV AX, 1
+	CWD
+	DIV BX
+	MOV z, AX
+	MOV AL, a
+	MUL a
+	MOV BX, AX
+	MOV AL, b
+	MUL b
+	SUB BX, AX
+	ADD BX, 4
+	MOV AX, 2
+	CWD
+	DIV BX
+	ADD AX, z
+	MOV z, AX	
+	
+	mov ax, 4C00h;
+	int 21h
+text ENDS
 END start
