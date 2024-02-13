8e545d7c70812b82334bffa0f1aa835fd14e1020
Raoul Nicolaescu <raoulg13@yahoo.com>
2024-02-13 17:47:29 +0200

    Operations on rows

---
+++ Exercises/OP_rows/ex1/ex1.asm
@@ -0,0 +1,32 @@
+assume cs:text,ds:data
+data SEGMENT
+
+s db 1,2,3,4
+ls equ $-s
+d dw ls-1 dup(?)
+
+data ENDS
+text SEGMENT
+
+	start:
+
+	MOV AX, data
+	MOV DS, AX
+
+	MOV CX, ls 
+	XOR SI,SI
+	XOR DI,DI
+
+	repeta:
+	MOV AL, [s+SI]
+	MUL [s+SI+1]
+	MOV [d+DI], AX
+	INC SI
+	ADD DI, 2
+	loop repeta
+	
+	
+	mov ax, 4C00h;
+	int 21h
+text ENDS
+END start
