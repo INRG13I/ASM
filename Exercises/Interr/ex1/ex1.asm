437cf4b5bca4d3709e0cf0675636fa03231fe5f1
Raoul Nicolaescu <raoulg13@yahoo.com>
2024-02-16 13:42:45 +0200

    added "you are given a string of bytes. show on the screen the string in base 2"

--- Exercises/Interr/ex1/ex1.asm
+++ Exercises/Interr/ex1/ex1.asm
@@ -1,5 +1,12 @@
+;Se da un sir de octeti in segmentul de date. Sa se afiseze elementele acestui sir la iesirea standard (ecran) in baza 2.
+
 assume cs:text,ds:data
 data SEGMENT
+
+s db 1, 2, 3, 4
+ls equ $-s
+
+
 
 
 data ENDS
@@ -10,7 +17,28 @@ text SEGMENT
 	MOV AX, data
 	MOV DS, AX
 
+	MOV CX, ls
+	XOR SI, SI
+
+	for:
+	MOV BL, byte ptr [s + SI]
+	PUSH CX
+	MOV CX, 8
+	for_loop:
+	XOR DL, DL
+	SHL BL,1
+	ADC DL, '0' 
+	MOV AH,02h
+	INT 21h
+	loop for_loop
+
+	MOV DL, ' '
+	MOV AH, 02h
+	int 21h 
 	
+	POP CX
+	INC SI	
+	loop for
 
 	
 	mov ax, 4C00h;
